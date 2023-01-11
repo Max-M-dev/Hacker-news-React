28ff9f623a2ea891277401cef5d258de54d2cbae
root <root@kvm-template-ubuntu>
2023-01-11 19:33:18 +0000

    chore: set env cache request

--- src/controller/request.js
+++ src/controller/request.js
@@ -2,5 +2,9 @@ import requestFetch from "../model/request/fetch";
 import cacheRequest from "../model/request/cache";
 
 export default async function request(url, params = {}) {
-	return await cacheRequest(url, params);
+  if (import.meta.env?.VITE_CACHE_REQUEST) {
+    return await cacheRequest(url, params);
+  } else {
+    return await requestFetch(url, params);
+  }
 }
